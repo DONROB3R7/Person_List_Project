d2467934964ddd0afcef73457294b861d88eed71
DONROB3R7 <costache.robert.valentin@gmail.com>
2019-07-03 18:54:26 +0200

    Complete v1
    Adding the core css  and also add a componet called Person. Through the component we are passing 4 props witch are make the component different from each other.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,32 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const Person = ({ img, name, job, children }) => {
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  return (
+    <article className="person">
+      <img src={url} alt="person" />
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
+    </article>
+  );
+};
+
 const PersonList = () => {
-  return <section>Persones </section>;
+  return (
+    <section className="person-list">
+      <Person img="32" name="John" job="Developer">
+        <ul>
+          <li>Html/Css</li>
+          <li>JavaScript/jQuery</li>
+          <li>CSS Preprocessing</li>
+        </ul>
+      </Person>
+      <Person img="22" name="John" job="Ceo" />
+      <Person img="5" name="John" job="Designer" />
+    </section>
+  );
 };
 
 const rootElement = document.getElementById("root");
